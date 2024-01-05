b4277c5f95c4a7236fbf39eec84342f4c49032da
robiul210 <robiulstudies@gmail.com>
2024-01-05 23:11:04 +0600

    ui get api test

--- test/api_test.dart
+++ test/api_test.dart
@@ -17,24 +17,39 @@ void main() {
 
   group('UserRepository - ', () {
     group('getUser function', () {
-
       test(
         'given UserRepository class when getUser function is called and status code is 200 then a usermodel should be returned',
-            () async {
+        () async {
           // Arrange
           when(
-                () => mockHTTPClient.get(
+            () => mockHTTPClient.get(
               Uri.parse('https://jsonplaceholder.typicode.com/users/1'),
             ),
           ).thenAnswer((invocation) async {
             return Response('''
             {
-              "id": 1,
-               "name": "Leanne Graham",
-               "username": "Bret",
-               "email": "Sincere@april.biz",
-                "website": "hildegard.org"
-            }
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Bret",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  },
+  "phone": "1-770-736-8031 x56442",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+}
             ''', 200);
           });
           // Act
@@ -46,10 +61,10 @@ void main() {
 
       test(
         'given UserRepository class when getUser function is called and status code is not 200 then an exception should be thrown',
-            () async {
+        () async {
           // arrange
           when(
-                () => mockHTTPClient.get(
+            () => mockHTTPClient.get(
               Uri.parse('https://jsonplaceholder.typicode.com/users/1'),
             ),
           ).thenAnswer((invocation) async => Response('{}', 500));
