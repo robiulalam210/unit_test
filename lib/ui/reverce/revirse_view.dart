7a4abc7cb03e7b4c46ae5840aaadde37bdd625a1
robiul210 <robiulstudies@gmail.com>
2024-01-06 20:19:07 +0600

    reverse string

--- lib/ui/reverce/revirse_view.dart
+++ lib/ui/reverce/revirse_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../reviers/reviers_funcation.dart';
+
 class RevierseView extends StatefulWidget {
   const RevierseView({super.key});
 
@@ -7,14 +9,44 @@ class RevierseView extends StatefulWidget {
   State<RevierseView> createState() => _RevierseViewState();
 }
 
+TextEditingController controller = TextEditingController();
+
 class _RevierseViewState extends State<RevierseView> {
+  var _reveerse = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Container(
         child: Column(
-          children: [],
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(hintText: "Input Hint"),
+                controller: controller,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            MaterialButton(
+                onPressed: () {
+                  _reveerse = reverseString(controller.text);
+                  print(_reveerse);
+                },
+                child: Text("Save"))
+          ],
         ),
       ),
     );
---
+++ test/reverse_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unit_test/reviers/reviers_funcation.dart';
+
+void main() {
+  test("string reverse", () {
+
+    var data="abc";
+   var info= reverseString(data);
+
+   expect(info, "cba");
+
+
+  });
+}
